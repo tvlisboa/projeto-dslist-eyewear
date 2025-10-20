77ccf597a011d59d9191f53b3ed7da229bfb170d
tvlisboa <tvlisboa@hotmail.com>
2025-10-20 15:53:09 -0300

    implementando projections e sql query

---
+++ ds-list-clothes/src/main/java/org/serratec/dev/dslistclothes/projections/OculosMinProjection.java
@@ -0,0 +1,10 @@
+package org.serratec.dev.dslistclothes.projections;
+
+public interface OculosMinProjection {
+    Long getId();
+    String getName();
+    Integer getYear();
+    String getImgUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
